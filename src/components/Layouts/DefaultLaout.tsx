81facadd17a97e7d9534f743c490a7fcc468df8c
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 16:35:33 +0530

    project setup

---
+++ src/components/Layouts/DefaultLaout.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState, ReactNode } from "react";
+
+export default function DefaultLayout({
+
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <>
+
+    </>
+  );
+}
