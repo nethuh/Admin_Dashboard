3b39a0fa2956c5426958b34f6eb73bab99771831
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 19:57:15 +0530

    side bar btn done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import {MdList} from "react-icons/md";
+
 const Header = (props: {
     sidebarOpen: string | boolean | undefined;
     setSidebarOpen: (arg0: boolean) => void;
@@ -6,10 +8,19 @@ const Header = (props: {
         <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
             <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
                 <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
-                    <button>
-
+                    <button
+                        aria-controls="sidebar"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            props.setSidebarOpen(!props.sidebarOpen);
+                        }}
+                        className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+                    >
+                        <MdList size={20}/>
                     </button>
                 </div>
+
+
             </div>
         </header>
     );
