c884f8cf30891fedf1ff5a847ec7ad091a3c6daf
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 20:51:48 +0530

    dashboard started

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Dashboard: React.FC = () => {
+    return(
+        <>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+
+        </div>
+        </>
+    )
+}
