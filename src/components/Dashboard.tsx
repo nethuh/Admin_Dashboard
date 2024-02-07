02c4408f13a092d5ba72d156b8af04465ee6c5af
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 21:10:55 +0530

    updated

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Card from "@/components/Card";
 import {MdOutlinePeople, MdOutlineShoppingBag, MdRemoveRedEye, MdShoppingCart} from "react-icons/md";
+import ChartOne from "@/components/chart/ChartOne";
 
 const Dashboard: React.FC = () => {
     return (
@@ -20,6 +21,10 @@ const Dashboard: React.FC = () => {
                     <MdOutlinePeople size={20}/>
                 </Card>
 
+            </div>
+
+            <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+                <ChartOne />
             </div>
         </>
     );
