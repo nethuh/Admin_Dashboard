76fa39d588cfb8004afb3d46c52c772c778b3e12
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 17:30:48 +0530

    updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ import {usePathname} from "next/navigation";
 
 import {
     MdArrowLeft,
-    MdSettings, MdCalendarMonth, MdDashboard, MdPeople, MdTableChart,
+    MdSettings, MdCalendarMonth, MdDashboard, MdPeople, MdTableChart, MdBarChart,
 } from "react-icons/md";
 
 interface SidebarProps {
@@ -15,7 +15,7 @@ interface SidebarProps {
     setSidebarOpen: (arg: boolean) => void;
 }
 
-const Sidebar = ({sidebarOpen , setSidebarOpen}: SidebarProps) => {
+const Sidebar = ({sidebarOpen, setSidebarOpen}: SidebarProps) => {
     const pathname = usePathname();
     return (
         <aside
@@ -120,16 +120,27 @@ const Sidebar = ({sidebarOpen , setSidebarOpen}: SidebarProps) => {
                     {/* <!-- Others Group --> */}
 
                     <div>
-                        <h3>
-
+                        <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+                            OTHERS
                         </h3>
+
+                        <ul className="mb-6 flex flex-col gap-1.5">
+                            <li>
+                                <Link
+                                    href="/"
+                                    className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                                        pathname.includes("chart") && "bg-graydark dark:bg-meta-4"
+                                    }`}
+                                >
+                                    <MdBarChart size={20}/>
+                                    Chart
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </div>
-
         </aside>
-
-
     );
 }
 
