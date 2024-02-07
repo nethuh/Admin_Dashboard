ab5f727fde89fc4c8a708e4f4070c42fca935c57
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 20:45:22 +0530

    updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,12 +9,33 @@ interface CardDataStatsProps {
     children: ReactNode;
 }
 
-const Card: React.FC<CardDataStatsProps> = ({}) => {
+const Card: React.FC<CardDataStatsProps> = ({
+                                                children,
+                                                total,
+                                                rate,
+                                                levelUp,
+                                                levelDown,
+                                                title
+                                            }) => {
+
     return (
-        <div className="rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
-            <div>
+        <div
+            className="rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
+                {children}
+            </div>
+
+            <div className="mt-4 flex items-end justify-between">
+                <div>
+                    <h4 className="text-title-md font-bold text-black dark:text-white">
+                        {total}
+                    </h4>
+                    <span className="text-sm font-medium">{title}</span>
+                </div>
+
 
             </div>
+
         </div>
     )
 }
