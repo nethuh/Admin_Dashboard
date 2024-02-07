51a09e523a5818e92e5c14882e1a65f68143dbb1
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 17:24:00 +0530

    menu items added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ import {usePathname} from "next/navigation";
 
 import {
     MdArrowLeft,
-    MdBackup, MdDashboard,
+    MdBackup, MdCalendarMonth, MdDashboard,
 } from "react-icons/md";
 
 interface SidebarProps {
@@ -65,6 +65,21 @@ const Sidebar = ({sidebarOpen , setSidebarOpen}: SidebarProps) => {
                                 <MdDashboard size={20} />
                                 Dashboard
                             </Link>
+
+                            <li>
+                                <Link href="/"
+                                      className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                                          pathname.includes("calendar") &&
+                                          "bg-graydark dark:bg-meta-4"
+                                      }`}
+                                      >
+
+                                    <MdCalendarMonth size={20}/>
+                                    Calender
+                                </Link>
+                            </li>
+
+
                         </ul>
                     </div>
                 </nav>
