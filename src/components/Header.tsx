6c097ce09c7f630eb5f9800719cb3f2e53e65098
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-07 19:44:12 +0530

    Header Implemention started

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = (props : {
+    sidebarOpen: string | boolean | undefined;
+    setSidebarOpen: (arg0: boolean) => void;
+}) => {
+    return(
+        <header>
+
+        </header>
+    );
+};
+
+export default Header;
